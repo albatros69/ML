d22d5efc7bdd8f7ba73aaa9dce1b7e82dbd3ad24
Mathieu <albatros69+github@gmail.com>
2018-01-30 18:59:16 +0100

    Adaptations pour usine à WS

---
+++ budget/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 sw=4 ts=4 et:
+
+from flask import Flask
+app = Flask(__name__)
+
+import ws_budget
+
+#if __name__ == '__main__':
+#    app.run(port=5999, debug=True, host='localhost')
+
