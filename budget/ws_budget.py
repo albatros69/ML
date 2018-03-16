c9f2bcdc88100fcd6ee162549600463aaf44b415
Mathieu <albatros69@users.noreply.github.com>
2018-03-16 12:58:22 +0100

    Syntaxe plus pythonesque...

--- budget/ws_budget.py
+++ budget/ws_budget.py
@@ -18,13 +18,13 @@ with gzip.open('model-budget.pkl.gz', 'rb') as model_dump:
 
 def transform_budget_data(data, categ_words):
     # Transformation de la date
-    result = dict(zip(['jour', 'mois', 'année'], map(int, data['date'][:10].split('/'))))
+    result = dict(zip(['jour', 'mois', 'année'], [ int(d) for d in data['date'][:10].split('/') ]))
 
     # Calcul du montant de l'opération
     result['montant'] = float('0'+data['credit'].replace(',', '.')) - float('0'+data['debit'].replace(',', '.'))
 
     # Traitement du texte de description (selon les mots utilisés par le modèle)
-    words = map(lambda s: s.lower(), re.split('\W+', data["nature"]))
+    words = [ s.lower() for s in re.split('\W+', data["nature"]) ]
     for w in categ_words:
         if w in words:
             result[w] = 1
